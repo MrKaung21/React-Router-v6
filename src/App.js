5660db89f13ca18a1c35269ae19be0b50b891108
Kaung Myat Tun <kaungtun111@gmail.com>
2022-09-27 23:04:30 +0630

    react-router-v6

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import Error from './pages/Error'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>Navbar</nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='products' element={<Products />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <footer>footer</footer>
+    </BrowserRouter>
+  )
+}
+
+export default App;
