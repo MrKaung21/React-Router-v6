f34902dfed1b1ab8e369c1184419f60cd2a0e345
Kaung Myat Tun <kaungtun111@gmail.com>
2022-09-30 01:09:28 +0630

    Products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
+import products from '../data'
 
 const Products = () => {
   return (
     <>
       <section className='section'>
         <h2>products</h2>
-        <Link to='/' className='btn'>Back Home</Link>
+        <div className="products">
+          {products.map(product => {
+            return <article key={product.id}>
+              <h5>{product.name}</h5>
+              <Link to={`/products/${product.id}`} style={{ color: 'blue' }}>more info</Link>
+            </article>
+          })}
+        </div>
       </section>
       
     </>
